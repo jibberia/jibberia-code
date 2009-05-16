bce63a3af0412d88382aa1f78b578d329d229fe5
kevintcox <kevintcox@5188a770-36cf-11de-9210-07d028069593>
2009-05-16 20:17:45 +0000

    new project i hopefully won't abandon

---
+++ audio-match/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from numpy import *
+from scipy import * # after numpy to import scipy's fft
+from scikits import audiolab
+
+filename = 'audio/lv1.aif'
+fft_window_size = 512
+window_size = 2 * fft_window_size
+
+
+audiofile = audiolab.sndfile(filename)
+samples = audiofile.read_frames(audiofile.get_nframes())
+
+# pad to right size so we can reshape it easily
+extras = repeat(0, window_size - len(samples) % window_size)
+samples = append(samples, extras)
+
+fft_shape = (samples.size / window_size, window_size)
+fft_windows = map(fft, samples.reshape(fft_shape))
+
