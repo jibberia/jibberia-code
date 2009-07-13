27209a3ad887da50ef5e7f291051a1558c7beaac
kevintcox <kevintcox@5188a770-36cf-11de-9210-07d028069593>
2009-07-13 21:59:58 +0000

    bs checkin, easier than scp

--- trunk/global-symphony/SymphonyContributor/Classes/MainViewController.m
+++ trunk/global-symphony/SymphonyContributor/Classes/MainViewController.m
@@ -34,33 +34,34 @@
 
 // http://stackoverflow.com/questions/1010343/how-do-i-record-audio-on-iphone-with-avaudiorecorder
 - (void)record {
-/*
 	AVAudioSession *audioSession = [AVAudioSession sharedInstance];
 	NSError *err = nil;
+	
+	// enter record mode
 	[audioSession setCategory:AVAudioSessionCategoryRecord error:&err];
 	if (err) {
-        NSLog(@"audioSession: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
+        NSLog(@"audioSession error: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
         return;
 	}
+	
 	[audioSession setActive:YES error:&err];
-//	err = nil;
 	if (err) {
-        NSLog(@"audioSession: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
+        NSLog(@"audioSession error: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
         return;
 	}
 	
 	recordSetting = [[NSMutableDictionary alloc] init];
 	
-	[recordSetting setValue :[NSNumber numberWithInt:kAudioFormatLinearPCM] forKey:AVFormatIDKey];
+	[recordSetting setValue:[NSNumber numberWithInt:kAudioFormatLinearPCM] forKey:AVFormatIDKey];
 	[recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey]; 
-	[recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
-	
-	[recordSetting setValue :[NSNumber numberWithInt:16] forKey:AVLinearPCMBitDepthKey];
-	[recordSetting setValue :[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsBigEndianKey];
-	[recordSetting setValue :[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsFloatKey];
+	[recordSetting setValue:[NSNumber numberWithInt:2] forKey:AVNumberOfChannelsKey];
+	[recordSetting setValue:[NSNumber numberWithInt:16]  forKey:AVLinearPCMBitDepthKey];
+	[recordSetting setValue:[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsBigEndianKey];
+	[recordSetting setValue:[NSNumber numberWithBool:NO] forKey:AVLinearPCMIsFloatKey];
 	
 
 	// Create a new dated file
+	[NSDate now];
 	NSDate *now = [NSDate dateWithTimeIntervalSinceNow:0];
 	NSString *caldate = [now description];
 	recorderFilePath = [[NSString stringWithFormat:@"%@/%@.caf", DOCUMENTS_FOLDER, caldate] retain];
@@ -68,39 +69,38 @@
 	NSURL *url = [NSURL fileURLWithPath:recorderFilePath];
 	err = nil;
 	recorder = [[AVAudioRecorder alloc] initWithURL:url settings:recordSetting error:&err];
-	if(!recorder){
+	if (!recorder) {
         NSLog(@"recorder: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
         UIAlertView *alert =
-        [[UIAlertView alloc] initWithTitle: @"Warning"
-								   message: [err localizedDescription]
-								  delegate: nil
-						 cancelButtonTitle:@"OK"
-						 otherButtonTitles:nil];
+			[[UIAlertView alloc] initWithTitle:@"Warning"
+									   message:[err localizedDescription]
+									  delegate:nil
+							 cancelButtonTitle:@"OK"
+							 otherButtonTitles:nil];
         [alert show];
         [alert release];
         return;
 	}
 	
-	//prepare to record
+	// prepare to record
 	[recorder setDelegate:self];
 	[recorder prepareToRecord];
 	recorder.meteringEnabled = YES;
 	
-	BOOL audioHWAvailable = audioSession.inputIsAvailable;
-	if (! audioHWAvailable) {
+	if (!audioSession.inputIsAvailable) {
         UIAlertView *cantRecordAlert =
-        [[UIAlertView alloc] initWithTitle: @"Warning"
-								   message: @"Audio input hardware not available"
-								  delegate: nil
-						 cancelButtonTitle:@"OK"
-						 otherButtonTitles:nil];
+			[[UIAlertView alloc] initWithTitle:@"Warning"
+									   message:@"Audio input hardware not available"
+									  delegate:nil
+							 cancelButtonTitle:@"OK"
+							 otherButtonTitles:nil];
         [cantRecordAlert show];
         [cantRecordAlert release]; 
         return;
 	}
 	// start recording
-	[recorder recordForDuration:(NSTimeInterval) 10];
-*/
+	[recorder record];
+//	[recorder recordForDuration:(NSTimeInterval) 10];
 }
 
 /*
