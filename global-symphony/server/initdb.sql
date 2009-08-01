8d5e57cc278e74a8d591eb88aab99795d8e918af
kevintcox <kevintcox@5188a770-36cf-11de-9210-07d028069593>
2009-08-01 17:17:59 +0000

    roughly supporting uploads in the server

--- global-symphony/server/initdb.sql
+++ global-symphony/server/initdb.sql
@@ -1,5 +1,7 @@
 -- run this as root:
 -- $ mysql -uroot -p < initdb.sql
 
+drop database if exists symphony_server;
 create database symphony_server;
 grant all on symphony_server.* to 'symphony'@'localhost' identified by 'argon412';
+grant all on symphony_server.* to '%'@'localhost';
