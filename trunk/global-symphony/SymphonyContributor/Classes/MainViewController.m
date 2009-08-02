66ad8138bf1960a75a06a11cde907e38b50869d9
kevintcox <kevintcox@5188a770-36cf-11de-9210-07d028069593>
2009-08-02 06:10:19 +0000

    upload works; added ASI fun network classes; minor gui shiftage; merged with paul's recorder code

--- trunk/global-symphony/SymphonyContributor/Classes/MainViewController.m
+++ trunk/global-symphony/SymphonyContributor/Classes/MainViewController.m
@@ -11,7 +11,6 @@
 #import "AudioRecorder.h"
 #import "AudioPlayer.h"
 
-
 @implementation MainViewController
 
 @synthesize playBtn;
@@ -21,7 +20,6 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) {
         // Custom initialization
-		NSLog(@"custom init point?");
 		mAudioRecorder = [[AudioRecorder alloc] init];
 		mAudioPlayer = [[AudioPlayer alloc] init];
     }
@@ -42,37 +40,16 @@
 // http://stackoverflow.com/questions/936855/file-upload-to-http-server-in-iphone-programming
 - (IBAction)upload {
 	NSLog(@"upload");
-	NSString *urlString = @"http://localhost:8000/samples/add";
-	NSString *filename = @"test.wav";
-	
-	request= [[[NSMutableURLRequest alloc] init] autorelease];
-	[request setURL:[NSURL URLWithString:urlString]];
-	[request setHTTPMethod:@"POST"];
-	NSString *boundary = @"---------------------------14737809831466499882746641449";
-	NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
-	[request addValue:contentType forHTTPHeaderField: @"Content-Type"];
-	
-	NSMutableData *postbody = [NSMutableData data];
-	[postbody appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-	[postbody appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"file\"; filename=\"%@\"\r\n", filename] dataUsingEncoding:NSUTF8StringEncoding]];
-	[postbody appendData:[[NSString stringWithString:@"Content-Type: application/octet-stream\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
-	[postbody appendData:[NSData dataWithData:[NSData dataWithContentsOfURL:mRecordURL]]];
-	[postbody appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-	[request setHTTPBody:postbody];
-	
-	NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
-	NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
-	NSLog(returnString);
+
+	request = [ASIFormDataRequest requestWithURL:[NSURL URLWithString:@"http://jibberia.dyndns.org:8000/samples/add"]];
+	[request setFile:[mAudioRecorder filePathStr] forKey:@"file"];
+	[request setPostValue:@"TODO" forKey:@"name"];
+	[request start];
 }
 
-- (IBAction)loopSwitchPress:(UISwitch*)switchitem
+- (IBAction)loopSwitchPress:(UISwitch *)switchitem
 {
 	mAudioPlayer.looping = loopSwitch.on;
-}
-
-- (IBAction)test:(id)sender {
-	if (loopSwitch.on) NSLog(@"Yes, should loop");
-	else NSLog(@"No, should NOT loop.");
 }
 
 - (IBAction)play {//:(UIButton *)button {
@@ -81,7 +58,6 @@
 
 // http://stackoverflow.com/questions/1010343/how-do-i-record-audio-on-iphone-with-avaudiorecorder
 - (void)record {
-	
 	// Create a new dated file
 	int ts = (int)[[NSDate dateWithTimeIntervalSinceNow:0] timeIntervalSince1970];
 	NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
@@ -132,20 +108,6 @@
 		playBtn.enabled = YES;
 		uploadBtn.enabled = YES;
 	}
-	/*
-	NSFileManager *fm = [NSFileManager defaultManager];
-	
-	err = nil;
-	[fm removeItemAtPath:[url path] error:&err];
-	if(err)
-        NSLog(@"File Manager: %@ %d %@", [err domain], [err code], [[err userInfo] description]);
-	
-	
-	
-	UIBarButtonItem *startButton = [[UIBarButtonItem alloc] initWithTitle:@"Record" style:UIBarButtonItemStyleBordered  target:self action:@selector(startRecording)];
-	self.navigationItem.rightBarButtonItem = startButton;
-	[startButton release];
-	*/
 }
 
 
