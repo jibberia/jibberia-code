6017870eafb7cd79e8d3f4f9fef58a27784eefae
kevintcox <kevintcox@5188a770-36cf-11de-9210-07d028069593>
2009-08-02 16:08:28 +0000

    making urls work, skeleton of audio map

--- trunk/global-symphony/server/urls.py
+++ trunk/global-symphony/server/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from sample.views import *
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,6 +13,9 @@ urlpatterns = patterns('',
     (r'^samples/add', add_sample),
     (r'^samples/', include('sample.urls')),
     (r'^samples/random', random_sample),
+    
+    (r'^playback/rainbow', direct_to_template, {'template': 'sample/playback.html'}),
+    (r'^playback/audio-map', direct_to_template, {'template': 'sample/audio-map.html'}),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
