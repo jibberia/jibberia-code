f34b448a019c8269ac5367bf1665b07a2f440fd0
kevintcox <kevintcox@5188a770-36cf-11de-9210-07d028069593>
2009-08-02 07:58:22 +0000

    ready for productiolol

--- global-symphony/server/settings.py
+++ global-symphony/server/settings.py
@@ -11,7 +11,7 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'symphony_server'             # Or path to database file if using sqlite3.
 DATABASE_USER = 'symphony'             # Not used with sqlite3.
 DATABASE_PASSWORD = 'argon412'         # Not used with sqlite3.
@@ -80,5 +80,5 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.admin',
-	'server.sample',
+    'server.sample',
 )
