8920cf15b41401c6696ecd446bf58e6882a68ab7
kevintcox <kevintcox@5188a770-36cf-11de-9210-07d028069593>
2009-09-11 01:47:48 +0000

    works well; added file browser and some crap. NEXT: a log, then more drag + drop

--- trunk/QTChannelAssigner/AppDelegate.h
+++ trunk/QTChannelAssigner/AppDelegate.h
@@ -8,31 +8,23 @@
 	NSMatrix *saveOptionRadioGroup;
 	NSTextField *pathField;
 	NSTextField *statusMsg;
-	
-//	NSMutableArray				*_trackChannelLabelNames;
-//	NSMutableArray				*_trackChannelLabelsMenusArray;
-//	NSMutableArray				*_trackChannelLabelsIndexOfSelectedMenuItemArray;
-//	
-//	NSMutableArray				*_extractionChannelLabelNames;
-//	NSMutableArray				*_extractionLayoutMenuList;	
 }
 
 
 @property (nonatomic, retain) IBOutlet NSMatrix *saveOptionRadioGroup;
 @property (nonatomic, retain) IBOutlet NSTextField *pathField;
 @property (nonatomic, retain) IBOutlet NSTextField *statusMsg;
+
 - (IBAction)go:(id)sender;
 - (IBAction)pathFieldChanged:(id)sender;
+- (IBAction)browse:(id)sender;
 
 - (void)assignChannelsToFileAtPath:(NSString *)path;
-
+- (void)validateAndAddMoviePath:(NSString *)path;
+- (void)populateValidMoviePaths;
 - (void)bailWithMessage:(NSString *)msg;
-
-//- (void)createLabelsArray;
-//- (void)addLabelToLabelNamesArray:(NSMutableArray *)namesArray label:(AudioChannelLabel)thisLabel;
 
 void setAudioTrackChannelLayoutDiscrete(Track inTrack, AudioChannelLabel lbl);
 OSStatus getTrackLayoutAndSize(Track track,  UInt32* outLayoutSize, AudioChannelLayout** outLayout);
-
 
 @end
