e6a918aca1fcc585dc198414b47d81e4f5c49a7a
kevintcox <kevintcox@5188a770-36cf-11de-9210-07d028069593>
2009-09-11 01:06:51 +0000

    removing old junk

--- trunk/QTChannelAssigner/AppDelegate.m
+++ trunk/QTChannelAssigner/AppDelegate.m
@@ -1,13 +1,13 @@
 #import "AppDelegate.h"
-//#import "QuickTimeAudioUtils.h"
 
 @implementation AppDelegate
+
 @synthesize saveOptionRadioGroup;
 @synthesize pathField;
 @synthesize statusMsg;
 
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	NSLog(@"applicationDidFinishLaunching");
 	validMoviePaths = nil;
 }
 
@@ -55,12 +55,6 @@
 	NSLog(msg);
 	[statusMsg setStringValue:msg];
 }
-
-//- (void)bailWithMessage:(NSString *)msg, ... {
-//	NSLog(msg);
-//	[statusMsg setStringValue:msg];
-//}
-
 
 - (IBAction)go:(id)sender {
 	NSString *path = [pathField stringValue];
@@ -207,134 +201,8 @@ void setAudioTrackChannelLayoutDiscrete(Track inTrack, AudioChannelLabel lbl)
     free((void *)trackChannelLayout);
 }
 
-
 - (void)dealloc {
-//	if (movie) [movie release], movie = nil;
 	[super dealloc];
 }
-
-/*
- 
- 
- // Populate the track selector pop-up button  
- - (void)populateTrackSelectorPopUpButton
- {
- UInt32 index;
- NSArray	*arrayOfMovieTracks;
- 
- [_audTrackSelectorPopUpButton removeAllItems];
- //	[self setTrack:nil];
- 
- if (movie == nil)
- return;
- 
- // First, add the special item associated with the movie summary mix. 
- // This item does not have a track associated with it
- [_audTrackSelectorPopUpButton insertItemWithTitle:@"Summary Channel Layout" atIndex:0];
- [[_audTrackSelectorPopUpButton itemAtIndex:0] setRepresentedObject:nil];
- [_audTrackSelectorPopUpButton selectItemAtIndex:0];
- 
- // Add to the pop-up menu, all the sound tracks of this movie
- arrayOfMovieTracks = [_currentMovie tracks];
- for (index = 0; index < [arrayOfMovieTracks count]; index++) 
- {
- if (trackMixesToAudioContext([[arrayOfMovieTracks objectAtIndex:index] quickTimeTrack]))
- {
- NSMutableString	*trackNumber;
- trackNumber = [[NSMutableString alloc] initWithFormat:@"Track %d: %@", index + 1,
- [[arrayOfMovieTracks objectAtIndex:index] attributeForKey:@"QTTrackDisplayNameAttribute"]];
- [_audTrackSelectorPopUpButton addItemWithTitle:trackNumber];
- [[_audTrackSelectorPopUpButton lastItem] setRepresentedObject:(QTTrack*)[arrayOfMovieTracks objectAtIndex:index]];
- [trackNumber release];	
- }
- }
- }
-
-
-
-// Create and add an InfoObject to the provided array
-- (void)addLabelToLabelNamesArray:(NSMutableArray *)namesArray label:(AudioChannelLabel)thisLabel
-{
-	InfoObject *labelInfo = [[InfoObject alloc] init];
-	NSString *labelStr = [NSString stringWithString:@""];
-	UInt32 labelSize = sizeof(NSString*);
-	AudioChannelDescription acd = {0};
-	acd.mChannelLabel = thisLabel;
-	(void)AudioFormatGetProperty(kAudioFormatProperty_ChannelName, 
-								 sizeof(AudioChannelDescription), 
-								 &acd, 
-								 &labelSize, 
-								 &labelStr);
-	[labelInfo setItem:thisLabel andItemName:labelStr];
-	[labelStr release];
-	[namesArray addObject:labelInfo];
-	[labelInfo release];
-}
-
-- (void)createLabelsArray
-{
-	// This tag gets us most of the labels that we are interested in
-	AudioChannelLayoutTag	tag = kAudioChannelLayoutTag_MPEG_7_1_A;
-	UInt32					layoutSize;
-	AudioChannelLayout		*layout;
-	UInt32					channel = 0;
-	UInt32					numDiscreteChannels = 0;
-	UInt32					numDeviceChannels;
-	UInt32					index;
-	OSStatus				err;
-	
-	if (noErr != AudioFormatGetPropertyInfo (kAudioFormatProperty_ChannelLayoutForTag, sizeof(AudioChannelLayoutTag), 
-											 &tag, &layoutSize))
-		goto bail;									
-	
-	layout = (AudioChannelLayout*)malloc(layoutSize);
-	if (noErr != AudioFormatGetProperty(kAudioFormatProperty_ChannelLayoutForTag, sizeof(AudioChannelLayoutTag), 
-										&tag, &layoutSize, layout))
-		goto bail;
-	
-	if (noErr != expandChannelLayout(&layout, &layoutSize))
-		goto bail;
-	
-	// Commonly used labels 
-	for (channel = 0 ; channel < layout->mNumberChannelDescriptions; channel++)
-	{
-		AudioChannelLabel thisLabel = (AudioChannelLabel)(layout->mChannelDescriptions[channel]).mChannelLabel;
-		[self addLabelToLabelNamesArray:_trackChannelLabelNames label:thisLabel];
-		[self addLabelToLabelNamesArray:_extractionChannelLabelNames label:thisLabel];
-	}
-	
-	// Center Surround
-	[self addLabelToLabelNamesArray:_trackChannelLabelNames label:kAudioChannelLabel_CenterSurround];
-	[self addLabelToLabelNamesArray:_extractionChannelLabelNames label:kAudioChannelLabel_CenterSurround];
-	
-	// Mono
-	[self addLabelToLabelNamesArray:_trackChannelLabelNames label:kAudioChannelLabel_Mono];
-	
-	// Discrete Labels
-	//	if (_deviceLayout)
-	//		numDeviceChannels = _deviceLayout->mNumberChannelDescriptions;
-	
-	AudioChannelLayout *localDiscreteLayout;	
-	err = getDiscreteExtractionLayout([movie quickTimeMovie], nil, &localDiscreteLayout);		
-	if (!err)
-		numDiscreteChannels = localDiscreteLayout->mNumberChannelDescriptions;
-	if (localDiscreteLayout)
-		free(localDiscreteLayout);
-	
-	numDiscreteChannels = (numDeviceChannels > numDiscreteChannels) ? numDeviceChannels : numDiscreteChannels;
-	
-	for (index = 0; index < numDiscreteChannels; index++)
-	{
-		AudioChannelLabel discreteLabel = (1L<<16) | index;
-		[self addLabelToLabelNamesArray:_trackChannelLabelNames label:discreteLabel];
-	}	
-	// Unused (to disable output from this channel)
-	[self addLabelToLabelNamesArray:_trackChannelLabelNames label:kAudioChannelLabel_Unused];
-	
-bail:
-	if (layout)
-		free(layout);
-}
- */
 
 @end
