eb7395ac7e4db8f89ef0debd75b10a5e3b1af7d7
kevintcox <kevintcox@5188a770-36cf-11de-9210-07d028069593>
2010-02-18 16:10:08 +0000

    fixing install and launch scripts

--- trunk/dotfiles/install.sh
+++ trunk/dotfiles/install.sh
@@ -17,26 +17,28 @@ fi
 
 PROFILE_FILE="$HOME/.bash_profile"
 
-if [ -e "$PROFILE_FILE" ]; then
-    if [ ! -h "$PROFILE_FILE" ]; then
-        echo "~/.bash_profile is not a symlink, save backup:"
-        echo "mv $PROFILE_FILE $PROFILE_FILE.someolderversion"
-        mv "$PROFILE_FILE" "$PROFILE_FILE.someolderversion"
-        echo "make symlink:"
-        echo "ln -s $CWD/.bash_profile $HOME/.bash_profile"
-        ln -s "$CWD/.bash_profile" "$HOME/.bash_profile"
-    else
-        echo "profile symlinks look ok, probably..."
-    fi
-    if [ ! -h "$HOME/.jibberia_bash" ]; then
-        echo "symlinking .jibberia_bash dir:"
-        echo "ln -s $CWD/.jibberia_bash $HOME/.jibberia_bash"
-        ln -s "$CWD/.jibberia_bash" "$HOME/.jibberia_bash"
-    else
-        echo "script dir symlinks look ok, in all likelihood..."
-    fi
-    echo "done."
+if [[ -e "$PROFILE_FILE" && ! -h "$PROFILE_FILE" ]]; then
+    echo "~/.bash_profile is not a symlink, save backup:"
+    echo "mv $PROFILE_FILE $PROFILE_FILE.someolderversion"
+    mv "$PROFILE_FILE" "$PROFILE_FILE.someolderversion"
 else
-    echo "bad"
+    echo "symlink exists, but we'll change it to this dir ($CWD)"
+    echo "rm $PROFILE_FILE"
+    rm "$PROFILE_FILE"
 fi
+
+echo "make symlink:"
+echo "ln -s $CWD/.bash_profile $HOME/.bash_profile"
+ln -s "$CWD/.bash_profile" "$HOME/.bash_profile"
+
+#if [ -e "$HOME/.jibberia_bash" ]; then
+    echo "re-symlinking .jibberia_bash dir:"
+    echo "rm $HOME/.jibberia_bash" 
+    rm "$HOME/.jibberia_bash" 
+    echo "ln -s $CWD/.jibberia_bash $HOME/.jibberia_bash"
+    ln -s "$CWD/.jibberia_bash" "$HOME/.jibberia_bash"
+#else
+#     echo "script dir symlinks look ok, in all likelihood... TODO could be wrong"
+#fi
+echo "done."
 
