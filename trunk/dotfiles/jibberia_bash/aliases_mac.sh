d14ea4092f9ef8d23a865d7eea55138c52a4d333
kevintcox <kevintcox@5188a770-36cf-11de-9210-07d028069593>
2010-07-02 19:24:01 +0000

    added property list editor alias

--- trunk/dotfiles/jibberia_bash/aliases_mac.sh
+++ trunk/dotfiles/jibberia_bash/aliases_mac.sh
@@ -14,5 +14,7 @@ alias prev='osascript -e "tell application \"iTunes\" to previous track"'
 
 alias mh='sed "s/\\\040/ /g" /Users/kevin/.mysql_history'
 
+alias plisteditor='open -a "Property List Editor"'
+
 # To create the locate database (/var/db/locate.database):
 # sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist
