da42ce04dedea73bf36338267720accdb56b598c
Kevin Cox <kevintcox@gmail.com>
2015-07-01 08:55:42 -0700

    keep vimrc in dotfiles (finally)

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -10,9 +10,10 @@ if [[ -e "$PROFILE_PATH" ]]; then
 	echo
 	echo "source \"$PROFILE_PATH\""
 	echo
-    echo "Then do this:"
+    echo "Then do these:"
     echo
     echo "cp jibberia_bash/sl.txt ~/.sl.txt"
+    echo 'ln -s "$(pwd)/vimrc" "$HOME/.vimrc"'
     echo
 else
 	echo "Run ./install.sh from your jibberia-code dotfiles directory"
