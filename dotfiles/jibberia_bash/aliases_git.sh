d86dc664e949c08277ec5da4502a7772a8f792fe
Kevin Cox <kevintcox@gmail.com>
2017-03-23 12:45:43 -0400

    add fun git aliases

--- dotfiles/jibberia_bash/aliases_git.sh
+++ dotfiles/jibberia_bash/aliases_git.sh
@@ -2,3 +2,6 @@ alias gs="git status"
 alias gd="git diff"
 alias gdc="git diff --cached"
 alias gds="git diff -- '*.cs'"
+alias git-rev='git rev-parse --short HEAD'
+alias gh="git remote get-url origin | awk -F[:.] '{print \"https://github.com/\"\$3}' | xargs open"
+alias ghb="open https://github.com/`git remote get-url origin | awk -F[:.] '{print $3}'`/tree/`git status --branch --porcelain | awk -F[\ .] '{print $2}'`"
