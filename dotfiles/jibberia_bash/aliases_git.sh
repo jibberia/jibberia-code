1fcbc8d18f83a42da3a94d37dd2d5b3bca164fb1
Kevin Cox <kevintcox@gmail.com>
2017-03-23 14:12:45 -0400

    alias -> fn in aliases file

--- dotfiles/jibberia_bash/aliases_git.sh
+++ dotfiles/jibberia_bash/aliases_git.sh
@@ -4,4 +4,6 @@ alias gdc="git diff --cached"
 alias gds="git diff -- '*.cs'"
 alias git-rev='git rev-parse --short HEAD'
 alias gh="git remote get-url origin | awk -F[:.] '{print \"https://github.com/\"\$3}' | xargs open"
-alias ghb="open https://github.com/`git remote get-url origin | awk -F[:.] '{print $3}'`/tree/`git status --branch --porcelain | awk -F[\ .] '{print $2}'`"
+ghb () {
+    open "https://github.com/`git remote get-url origin | awk -F[:.] '{print $3}'`/tree/`git status --branch --porcelain | awk -F[\ .] '{print $2}'`"
+}
