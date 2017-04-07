f18233a296517629cf073afbe2fb1144c0877757
Kevin Cox <kevintcox@gmail.com>
2017-04-07 11:18:00 -0400

    Fix ghb

--- dotfiles/jibberia_bash/aliases_git.sh
+++ dotfiles/jibberia_bash/aliases_git.sh
@@ -5,5 +5,5 @@ alias gds="git diff -- '*.cs'"
 alias git-rev='git rev-parse --short HEAD'
 alias gh="git remote get-url origin | awk -F[:.] '{print \"https://github.com/\"\$3}' | xargs open"
 ghb () {
-    open "https://github.com/`git remote get-url origin | awk -F[:.] '{print $3}'`/tree/`git status --branch --porcelain | awk -F[\ .] '{print $2}'`"
+    open "https://github.com/`git remote get-url origin | awk -F[:.] '{print $3}'`/tree/`git status --branch --porcelain | head -n 1 | awk -F[\ .] '{print $2}'`"
 }
